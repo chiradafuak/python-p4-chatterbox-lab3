2f0a612ed73fc9e7ebc5f79ebe7e589137406fea
Ian <ian.githae2@student.moringaschool.com>
2025-06-19 16:23:08 +0300

    new files

--- server/models.py
+++ server/models.py
@@ -12,3 +12,10 @@ class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(80))
+    username = db.Column(db.String(80))
+    created_at = db.Column(db.DateTime, default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    def __repr__(self):
+        return f"<Message by {self.username}: {self.body}>"
